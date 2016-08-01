e481110eed523a1c6dc3e1a8d152785ab909cfc9
Seth Battis <sethbattis@stmarksschool.org>
2016-08-01 09:20:55 -0400

    BootstrapSmarty 2.0/PSR-2
    Updated to reflect the new simplified `battis/bootstrapsmarty`

--- src/StMarksSmarty.php
+++ src/StMarksSmarty.php
@@ -1,5 +1,7 @@
 <?php
 
+/** StMarksSmarty and related classes */
+
 namespace smtech\StMarksSmarty;
 
 use Battis\BootstrapSmarty\BootstrapSmarty;
@@ -12,66 +14,92 @@ use Battis\DataUtilities;
  **/
 class StMarksSmarty extends BootstrapSmarty
 {
+	/**
+	 * Identifier for UI layer provided by this class
+	 */
+	const KEY = 'StMarks-BootstrapSmarty';
 
-    const KEY = 'StMarks-BootstrapSmarty';
+	/**
+	 * Is this app presented inside of an `iframe`?
+	 * @var boolean
+	 */
+	private $isFramed = false;
 
-    private $isFramed = false;
+	/**
+	 * @inheritDoc
+	 *
+	 * @param string $template
+	 * @param string $config
+	 * @param string $compile
+	 * @param string $cache
+	 * @return StMarksSmarty
+	 */
+	public static function getSmarty($template = null, $config = null, $compile = null, $cache = null)
+	{
+		if (self::$singleton === null) {
+			self::$singleton = new self($template, $config, $compile, $cache);
+		}
+		return self::$singleton;
+	}
 
-    /**
-     * @inheritDoc
-     *
-     * @param string $template
-     * @param string $config (Optional)
-     * @param string $compile (Optional)
-     * @param string $cache (Optional)
-     */
-    public function __construct($template = null, $config = null, $compile = null, $cache = null)
-    {
-        parent::__construct($template, $config, $compile, $cache);
+	/**
+	 * @inheritDoc
+	 *
+	 * @param string $template
+	 * @param string $config
+	 * @param string $compile
+	 * @param string $cache
+	 */
+	public function __construct($template = null, $config = null, $compile = null, $cache = null)
+	{
+		parent::__construct($template, $config, $compile, $cache);
 
-        $this->addTemplateDir(__DIR__ . '/../templates', self::KEY);
-        $this->addStylesheet(DataUtilities::URLfromPath(__DIR__ . '/../css/StMarksSmarty.css'), self::KEY);
-    }
+		$this->prependTemplateDir(__DIR__ . '/../templates', self::KEY);
+		$this->addStylesheet(
+			DataUtilities::URLfromPath(__DIR__ . '/../css/StMarksSmarty.css'),
+			self::KEY
+		);
+	}
 
-    /**
-     * Is this app displayed inside an `IFRAME`?
-     *
-     * @param boolean $isFramed
-     */
-    public function setFramed($isFramed)
-    {
-        $this->isFramed = ($isFramed == true);
-    }
+	/**
+	 * Set whether or not this app is in an `iframe`
+	 *
+	 * @param boolean $isFramed
+	 */
+	public function setFramed($isFramed)
+	{
+		$this->isFramed = (bool) $isFramed;
+	}
 
-    /**
-     * Is this app displayed inside an `IFRAME`?
-     *
-     * @return boolean
-     */
-    public function isFramed()
-    {
-        return $this->isFramed;
-    }
+	/**
+	 * Is this app presented in an `iframe`?
+	 *
+	 * @return boolean
+	 */
+	public function isFramed()
+	{
+		return $this->isFramed;
+	}
 
-    /**
-     * @inheritDoc
-     *
-     * @param string $template (Optional)
-     * @param string $cache_id (Optional)
-     * @param string $compile_id (Optional)
-     * @param string $parent (Optional)
-     * @return void
-     */
-    public function display($template = 'page.tpl', $cache_id = null, $compile_id = null, $parent = null)
-    {
-        if ($this->isFramed()) {
-            $this->addStylesheet(
-                DataUtilities::URLfromPath(__DIR__ . '/../css/StMarksSmarty.css') . '?isFramed=true',
-                self::KEY
-            );
-        }
-        $this->assign('isFramed', $this->isFramed());
+	/**
+	 * @inheritDoc
+	 *
+	 * @param string $template
+	 * @param string $cache_id
+	 * @param string $compile_id
+	 * @param string $parent
+	 * @return void
+	 */
+	public function display($template = 'page.tpl', $cache_id = null, $compile_id = null, $parent = null)
+	{
+		if ($this->isFramed()) {
+			$this->addStylesheet(
+				DataUtilities::URLfromPath(__DIR__ . '/../css/StMarksSmarty.css') . '?isFramed=true',
+				self::KEY
+			);
+		}
+		$this->assign('isFramed', $this->isFramed());
 
-        parent::display($template, $cache_id, $compile_id, $parent);
-    }
+		parent::display($template, $cache_id, $compile_id, $parent);
+	}
 }
